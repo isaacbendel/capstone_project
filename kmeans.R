bf7896c9828114db6a37e9ef0760067f454d8038
b <b>
2016-11-15 11:20:39 -0500

    k-means finnished, still working on knn

---
+++ kmeans.R
@@ -0,0 +1,74 @@
+library(RColorBrewer)
+setwd('/home/bendel/R/Hand/')
+getwd()
+hand <-read.csv('train.csv')
+nrow(hand)
+#Split data into training set and testing set
+train<-hand[1:30000,]
+test<-hand[30001:42000,]
+b<-colorRampPalette(brewer.pal(9,"Blues"))(100)
+
+#This will show us an image of the n-th digit in the training set
+n<-1
+image(matrix(as.integer(train[n,2:785]), nrow = 28, byrow=FALSE)[,c(seq(28,1))],col = b)
+colors()[grep('grey', colors())]
+#Use k-means clustering on training set, group items into 10 clusters
+#Took about one minute to run
+set.seed(20)
+clust<-kmeans(train, 10)
+#These are the centroids
+par(mfrow=c(2,5))
+for (i in seq(1,10)){
+  image(matrix(clust$centers[i,2:785], nrow = 28, byrow=FALSE)[,c(seq(28,1))], col = b, axes = FALSE, xlab = paste('Cluster number', i, sep = ' '))
+}
+
+#Doesn't look too obvious what some of these are meant to be.
+
+#Try a different seed
+set.seed(22)
+clust<-kmeans(train, 10)
+
+#These are the centroids
+for (i in seq(1,10)){
+  image(matrix(clust$centers[i,2:785], nrow = 28, byrow=FALSE)[,c(seq(28,1))], xlab = paste('Cluster number', i, sep = ' '))
+}
+
+#Try using 20 different clusters
+k<-20
+set.seed(20)
+clust<-kmeans(train, k)
+
+#These are the centroids
+for (i in seq(1,k)){
+  image(matrix(clust$centers[i,2:785], nrow = 28, byrow=FALSE)[,c(seq(28,1))], xlab = paste('Cluster number', i, sep = ' '))
+}
+
+#Try using 20 different clusters
+k<-40
+set.seed(20)
+clust<-kmeans(train, 20, iter.max = 20)
+
+?kmeans
+
+
+#These are the centroids
+par(mfrow = c(2,5))
+for (i in seq(1,10)){
+  image(matrix(clust$centers[i,2:785], nrow = 28, byrow=FALSE)[,c(seq(28,1))], xlab = paste(paste('Cluster number', i, sep = ' '), clust$centers[i,1], sep = ' '), col = b, axes = FALSE)
+}
+par(mfrow = c(2,5))
+for (i in seq(11,20)){
+  image(matrix(clust$centers[i,2:785], nrow = 28, byrow=FALSE)[,c(seq(28,1))], xlab = paste(paste('Cluster number', i, sep = ' '), clust$centers[i,1], sep = ' '), col = b, axes = FALSE)
+}
+
+
+
+
+
+
+for (i in seq(1,20)){
+  q<-matrix(clust$centers[i,2:785], nrow = 28, byrow=FALSE)
+  q[which(q<130)]<-0
+  image(q[,c(seq(28,1))], col = b, axes = FALSE, xlab = i)
+}
+
---
+++ knn.R
@@ -0,0 +1,144 @@
+#' # k Nearest Neighbour
+#' # 
+#+echo=FALSE
+library(formattable)
+library(ggplot2)
+setwd('/home/bendel/R/Hand/')
+source('showImage.R')
+source('confusionMatrix.R')
+library(class)
+
+#+echo = TRUE
+
+#'# Preparation
+#'# 
+#'## Load Data, and separate into train and test sets
+hand <-read.csv('train.csv')
+train<-hand[1:30000,]
+test<-hand[30001:42000,]
+
+#' # 
+#' # Run a k-Nearest Neighbour classification
+tm1 <- system.time(
+  {
+    a<-knn(train[1:1000,2:785], test[,2:785], train[1:1000,1])    
+  })
+
+#' # 
+#' # Result
+#' ## Time to run
+tm1
+
+#' # 
+#' ## Confusion Matrix
+confusionMatrix(test[,1],a)
+
+#'# Analysis
+#'
+#' I tried running the knn on the entire train and test set. I was taking a long time. I tried with only 1000 from training set. It took about 2 minutes. k-means took only a few seconds to process the entire train and test set. This result was found using the default value of k = 1. We can now try playing with value of k, as well as considering greater slice of training set.
+
+#'# 
+#'# Round 2
+#'# 
+tm1 <- system.time(
+  {
+    a<-knn(train[1:1000,2:785], test[,2:785], train[1:1000,1], k = 5)
+  })
+
+#' ## Time to run
+
+tm1
+
+#' ## Confusion Matrix
+
+confusionMatrix(test[,1],a)
+
+#' # Analysis
+#' The accuracy actually decreased slightly by using k = 5.
+
+#' # Round 3
+
+#' Until now I just took the first 1000 images from the test set. Now I'll try randomly selecting 100 of each image.
+
+digits = list()
+for (i in seq(1,10)){
+  digits[[i]] <- train[which(train$label == i%%10),]
+}
+
+set.seed(20)
+randomList<-sample(1:2700,100)
+
+newDigits<-data.frame()
+for (i in seq(1,10)){
+  newDigits<-rbind.data.frame(newDigits,digits[[i]][randomList,1:785])
+}
+
+accuracy<-vector()
+for (i in seq(1, 21, 2)){
+  tm1 <- system.time(
+  {
+    a <-knn(newDigits[,2:785], test[,2:785], newDigits[,1], k = i)
+  })
+  print(c(i,tm1[3]))
+  accuracy<-c(accuracy, confusionMatrix(test[,1], a, justAccuracy = TRUE))
+}
+
+#+echo+TRUE
+q<- cbind.data.frame(seq(1,21,2), accuracy)
+names(q) <- c('k', 'Accuracy')
+ggplot(data = q, aes(x = k, y = Accuracy)) + geom_line() + geom_point()
+
+#' It's fascinating that the accuracy goes down as we consider more neighbours.
+#' 
+#' # Round 4
+#' Now try 200 from each digit.
+
+set.seed(20)
+randomList<-sample(1:2700,200)
+
+newDigits<-data.frame()
+for (i in seq(1,10)){
+  newDigits<-rbind.data.frame(newDigits,digits[[i]][randomList,1:785])
+}
+
+accuracy<-vector()
+for (i in seq(1, 21, 2)){
+  tm1 <- system.time(
+    {
+      a <-knn(newDigits[,2:785], test[,2:785], newDigits[,1], k = i)
+    })
+  print(c(i,tm1[3]))
+  accuracy<-c(accuracy, confusionMatrix(test[,1], a, justAccuracy = TRUE))
+}
+
+q<- cbind.data.frame(seq(1,21,2), accuracy)
+names(q) <- c('k', 'Accuracy')
+ggplot(data = q, aes(x = k, y = Accuracy)) + geom_line() + geom_point()
+
+#' Interestingly enough - for the first time we get better accuracy when k goes from 1 to 2. Also we have broken the 90% accuracy mark for the first time.
+
+#' # Round 5
+#' Let's see how accuracy is affected by how many selections we make from each digit. We will keep k constant at k = 1.
+
+set.seed(20)
+accuracy<-vector()
+for (i in seq(100, 1000,100)) {
+  randomList<-sample(1:2700,i)
+  
+  newDigits<-data.frame()
+
+  for (i in seq(1,10)){
+    newDigits<-rbind.data.frame(newDigits,digits[[i]][randomList,1:785])
+  }
+  
+  tm1 <- system.time(
+    {
+      a <-knn(newDigits[,2:785], test[,2:785], newDigits[,1], k = i)
+    })
+  print(c(i,tm1[3]))
+  accuracy<-c(accuracy, confusionMatrix(test[,1], a, justAccuracy = TRUE))
+}
+beepr::beep(8)
+q<- cbind.data.frame(seq(100,1000,100), accuracy)
+names(q) <- c('Number of samples from each digit', 'Accuracy')
+ggplot(data = q, aes(x = `Number of samples from each digit`, y = Accuracy)) + geom_line() + geom_point()
